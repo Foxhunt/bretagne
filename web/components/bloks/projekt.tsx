af169468fb4ed77c789c7fb147864c15ae28a72f
Marco Mojica <marco@mojica.de>
2023-01-28 23:36:55 +0100

    Add spacing to projekt page

--- web/components/bloks/projekt.tsx
+++ web/components/bloks/projekt.tsx
@@ -2,12 +2,16 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 
 export default function Projekt({ blok }) {
   return (
-    <main {...storyblokEditable(blok)} className="">
-      <h1>{blok.name}</h1>
-      <p>{blok.beschreibung}</p>
-      {blok.blocks.map((block) => (
-        <StoryblokComponent blok={block} key={block._uid} />
-      ))}
+    <main {...storyblokEditable(blok)} className="m-5">
+      <div className="flex flex-col place-content-end h-[40vh] ">
+        <h1 className="text-4xl">{blok.name}</h1>
+      </div>
+      <p className="my-4">{blok.beschreibung}</p>
+      <div className="flex flex-wrap items-center gap-4">
+        {blok.blocks.map((block) => (
+          <StoryblokComponent blok={block} key={block._uid} />
+        ))}
+      </div>
     </main>
   );
 }
