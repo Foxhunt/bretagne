c8c3561aa4307848d2a40e0bdce81251ed513213
Marco Mojica <marco@mojica.de>
2023-03-18 11:48:46 +0100

    Remove getStaticProps from index page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,12 +1,3 @@
-import { GetStaticProps } from "next";
-
 export default function Index() {
   return <></>;
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {},
-    revalidate: 3600, // revalidate every hour
-  };
-};
