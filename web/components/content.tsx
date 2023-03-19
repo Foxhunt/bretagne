df728872046b7c00be4ab3ba9ad03dba9b242187
Marco Mojica <marco@mojica.de>
2023-03-19 18:58:31 +0100

    Make last content BIG!

--- web/components/content.tsx
+++ web/components/content.tsx
@@ -2,7 +2,10 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 
 export default function Content({ blok }) {
   return (
-    <div {...storyblokEditable(blok)} className="flex justify-center">
+    <div
+      {...storyblokEditable(blok)}
+      className="flex justify-center md:last:col-span-2 md:last:row-span-2"
+    >
       <StoryblokComponent blok={blok} />
     </div>
   );
